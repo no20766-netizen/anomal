a3f1d85891e9bc0b2b6e56ec66b093a552ba6f7f
no20766-netizen <no20766@gmail.com>
2025-08-05 20:11:53 +0900

    Always serve media files in urls.py

--- anomal_store/urls.py
+++ anomal_store/urls.py
@@ -10,3 +10,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# DEBUG 상관없이 media를 서빙합니다
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
